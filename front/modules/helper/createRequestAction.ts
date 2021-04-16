9d1de052ea230b156c39d58273876b0c81a12a35
yjkwon07 <dudwns9073@nate.com>
2021-04-17 08:27:40 +0900

    [module-helper]: requestAction tunkAction 추가 - 로직 비동기 함수 처리도 한번에 생성하도록 묶음 - createRequestAsyncThunk Meta 타입 재 지정 - eslint: no-cycle => 무한으로 깊어질때만 에러처리

--- front/modules/helper/createRequestAction.ts
+++ front/modules/helper/createRequestAction.ts
@@ -1,15 +1,18 @@
 import { createAction } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
 
+import { createRequestAsyncThunk } from './createRequestAsyncThunk';
+
 export const createRequestAction = <R, S, F = AxiosError, M = any>(type: string) => {
   const REQUEST = `${type}/request`;
   const SUCCESS = `${type}/success`;
   const FAILURE = `${type}/fail`;
 
-  return {
+  const action = {
     TYPE: type,
     requset: createAction(REQUEST, (payload: R, meta?: M) => ({ payload, meta })),
     success: createAction(SUCCESS, (payload: S, meta?: M) => ({ payload, meta })),
     failure: createAction(FAILURE, (payload: F, meta?: M) => ({ payload, meta })),
   };
+  return { ...action, tunk: createRequestAsyncThunk(action.requset) };
 };
--- front/modules/helper/createRequestAsyncThunk.ts
+++ front/modules/helper/createRequestAsyncThunk.ts
@@ -2,7 +2,7 @@ import { Dispatch } from '@reduxjs/toolkit';
 
 import { ActionMetaPayload } from './type';
 
-export function createRequestAsyncThunk<P, R, M = any>(action: ActionMetaPayload<P, M>) {
+export function createRequestAsyncThunk<P, R, M = any>(action: ActionMetaPayload<P, M | undefined>) {
   return (payload: P, meta?: M) => (dispatch: Dispatch) => {
     return new Promise<R>((resolve, reject) => {
       dispatch({ ...action(payload, meta), resolve, reject });
