4830560924740920afa39a9fbf1500a6669160ac
yjkwon07 <dudwns9073@nate.com>
2021-04-25 11:49:57 +0900

    [router]: post - post 게시 - comment 게시

---
+++ back/routes/post.js
@@ -0,0 +1,79 @@
+import { SUCCESS } from '../../front/modules/fetchStatus';
+
+const express = require('express');
+
+const { Post, Image, Comment, User, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+const { SUCCESS, CLIENT_ERROR } = require('../constant');
+
+const router = express.Router();
+
+router.post('/', isLoggedIn, async (req, res, next) => {
+  // POST /post
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+    const fullPost = await Post.findOne({
+      where: { id: post.id },
+      include: [
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User, // 댓글 작성자
+              attributes: ['id', 'nickname'],
+            },
+          ],
+        },
+        {
+          model: User, // 게시글 작성자
+          attributes: ['id', 'nickname'],
+        },
+        {
+          model: User, // 좋아요 누른 사람
+          as: 'Likers',
+          attributes: ['id'],
+        },
+      ],
+    });
+    res.status(SUCCESS).send(fullPost);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// POST /post/1/comment
+router.post('/:postId/comment', isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId },
+    });
+    if (!post) {
+      return res.status(CLIENT_ERROR).send('존재하지 않는 게시글입니다.');
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: parseInt(req.params.postId, 10),
+      UserId: req.user.id,
+    });
+    const fullComment = await Comment.findOne({
+      where: { id: comment.id },
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'nickname'],
+        },
+      ],
+    });
+    res.status(SUCCESS).send(fullComment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
