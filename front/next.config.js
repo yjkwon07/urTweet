84de0b8d9400146d5d8d9ae3eaeac257e4c86dd7
yjkwon07 <dudwns9073@nate.com>
2021-05-23 19:25:32 +0900

    [build]: 배포 package 추가 - cross-env - bundle-analyzer

---
+++ front/next.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const newConfig = {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      plugins: [...config.plugins, new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)],
+    };
+    if (prod) {
+      newConfig.devtool = 'hidden-source-map';
+    }
+    return newConfig;
+  },
+});
