8fbf954eb2f16cb61b842450124d7bcbc54d49b7
yjkwon07 <dudwns9073@nate.com>
2021-06-19 15:39:59 +0900

    [PostCardMeta]: 리트윗으로 중복된 content 컴포넌트 모듈화

---
+++ front/components/PostCard/PostCardMeta/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Card, Tooltip } from 'antd';
+import Avatar from 'antd/lib/avatar/avatar';
+import moment from 'dayjs';
+import Link from 'next/link';
+
+import { GET_USER_URL, PASS_HREF } from '@utils/urls';
+
+import { Title } from './styles';
+
+export interface IProps {
+  userId: string;
+  nickname: string;
+  createdAt: string;
+  actions?: React.ReactNode;
+  description?: React.ReactNode;
+}
+
+const PostCardMeta = ({ userId, nickname, createdAt, actions, description }: IProps) => {
+  return (
+    <Card.Meta
+      avatar={
+        <Link href={GET_USER_URL(userId)} passHref>
+          <a href={PASS_HREF}>
+            <Avatar>{nickname}</Avatar>
+          </a>
+        </Link>
+      }
+      title={
+        <Title>
+          <div>
+            {nickname}
+            <Tooltip title={moment(createdAt).format('YYYY-MM-DD HH:mm:ss')}>
+              <span style={{ color: '#ccc', marginLeft: '10px', fontSize: '14px' }}>{moment(createdAt).fromNow()}</span>
+            </Tooltip>
+          </div>
+          {actions && <div>{actions}</div>}
+        </Title>
+      }
+      description={description}
+    />
+  );
+};
+
+export default PostCardMeta;
