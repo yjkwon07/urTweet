8b01dd94104d10b2dddde263c2f99cfe685d644f
yjkwon07 <dudwns9073@nate.com>
2021-07-04 18:12:01 +0900

    [module]: fetchStatus actionList 검사 - indexof => includes로 true, false 판단

--- front/modules/fetchStatus/hooks/useFetchStatus.ts
+++ front/modules/fetchStatus/hooks/useFetchStatus.ts
@@ -5,16 +5,18 @@ import { useAppSelector } from '@hooks/useAppRedux';
 import { FetchStatus } from '../slice';
 
 export default function useFetchStatus(type: string, actionId?: any) {
-  const [status, setStatus] = useState<FetchStatus | undefined>(undefined);
+  const [status, setStatus] = useState<FetchStatus>('INIT');
   const fetchStatus = useAppSelector((state) => state.FETCH_STATUS[type]?.status);
   const data = useAppSelector((state) => state.FETCH_STATUS[type]?.data);
   const actionList = useAppSelector((state) => state.FETCH_STATUS[type]?.actionList);
 
   useEffect(() => {
     if (actionId && actionList) {
-      if (actionList.indexOf(actionId) > -1) {
+      if (actionList.includes(actionId)) {
         setStatus(fetchStatus);
-      } else setStatus(undefined);
+      } else {
+        setStatus('INIT');
+      }
     } else {
       setStatus(fetchStatus);
     }
