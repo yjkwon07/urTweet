feefd179ae87efa8019cc10191299e309bbe944c
yjkwon07 <dudwns9073@nate.com>
2021-07-04 16:25:45 +0900

    [module]: configStore getDefaultMiddleware - redux-thunk 제거 툴킷에서 포함된 라이브러임 - AppDispach 타입 생성

--- front/modules/store/configStore.ts
+++ front/modules/store/configStore.ts
@@ -1,7 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { createWrapper, MakeStore } from 'next-redux-wrapper';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
 import createSagaMiddleware from 'redux-saga';
-import thunkMiddleware from 'redux-thunk';
 
 import rootSaga from './sagas';
 import rootReducer from './slices';
@@ -10,11 +9,11 @@ const devMode = process.env.NODE_ENV === 'development';
 
 // Next.js를 사용하게 되면 유저가 요청할 때 마다 redux store를 새로 생성하게 되므로 redux store가 여러 개가 될 수 있다.
 // getInitialProps, getServerSideProps 등에서 redux store에 접근할 수 있어야 하는데 그것을 next-redux-wrapper가 도와준다.
-const makeStore: MakeStore<RootState> = () => {
+const makeStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const store = configureStore({
     reducer: rootReducer,
-    middleware: [thunkMiddleware, sagaMiddleware],
+    middleware: [...getDefaultMiddleware(), sagaMiddleware],
     devTools: devMode,
   });
   // Next Redux Toolkit 에서 saga를 사용해야할 때
@@ -26,4 +25,5 @@ const wrapper = createWrapper(makeStore, {
   debug: devMode,
 });
 
+export type AppDispatch = ReturnType<typeof makeStore>['dispatch'];
 export default wrapper;
