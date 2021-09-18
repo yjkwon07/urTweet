9c71f4f13756c7c7157f42d441fa93614edb922b
yjkwon07 <dudwns9073@nate.com>
2021-09-18 14:57:04 +0900

    [layout]: 기본 레이아웃 스타일 변경 - filter가 있을 때, col 나눔

--- front/layouts/BaseLayout/index.tsx
+++ front/layouts/BaseLayout/index.tsx
@@ -1,67 +1,70 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC } from 'react';
 
-import { Global } from '@emotion/react';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu, Col, Layout, Row } from 'antd';
 import Link from 'next/link';
-import Router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
-import { useMyUser } from '@modules/user';
-import { PASS_HREF, HOME_URL, PROFILE_URL, GET_HASHTAG_URL } from '@utils/urls';
+import { getUserId } from '@utils/auth';
+import { PASS_HREF, HOME_URL, PROFILE_URL } from '@utils/urls';
 
 import LoginForm from './organism/LoginForm';
 import UserProfile from './organism/UserProfile';
-import { globalStyles, SearchInput } from './styles';
+import { StyledLayout } from './styles';
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
-const BaseLayout: FC = ({ children }) => {
+interface IProps {
+  filterGroup?: React.ReactNode;
+}
+
+const BaseLayout: FC<IProps> = ({ filterGroup, children }) => {
   const router = useRouter();
-  const { data: myData } = useMyUser({});
-
-  const [searchInput, setSearchInput] = useState('');
-
-  const handleChangeSearchInput = useCallback((e) => {
-    setSearchInput(e.target.value);
-  }, []);
-
-  const handleSearch = useCallback(() => {
-    Router.push(GET_HASHTAG_URL(searchInput));
-  }, [searchInput]);
 
   return (
-    <Layout className="layout">
-      <Global styles={globalStyles} />
-      <Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor: '#FFF' }}>
-        <Menu mode="horizontal" defaultSelectedKeys={[router.pathname]}>
-          <Menu.Item key={HOME_URL}>
-            <Link href={HOME_URL} passHref>
-              <a href={PASS_HREF}>urTweet</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key={PROFILE_URL}>
-            <Link href={PROFILE_URL} passHref>
-              <a href={PASS_HREF}>프로필</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/search">
-            <SearchInput enterButton value={searchInput} onChange={handleChangeSearchInput} onSearch={handleSearch} />
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px', marginTop: 64, backgroundColor: '#FFF' }}>
-        <div style={{ minHeight: 400, padding: 24 }}>
-          <Row gutter={12}>
-            <Col xs={24} sm={24} md={24} lg={8} style={{ paddingTop: 12 }}>
-              {myData ? <UserProfile /> : <LoginForm />}
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={9} style={{ paddingTop: 12 }}>
+    <StyledLayout>
+      <Content>
+        <div style={{ height: 50 }} />
+        <Row gutter={30}>
+          <Col className="header" xs={24} sm={24} md={24} lg={8}>
+            <div className="content">
+              {getUserId() ? <UserProfile /> : <LoginForm />}
+              <div className="blank" />
+              <Menu className="menu" mode="vertical" defaultSelectedKeys={[router.pathname]}>
+                <Menu.Item key={HOME_URL} className="menu-item">
+                  <Link href={HOME_URL} passHref>
+                    <a href={PASS_HREF}>
+                      <HomeOutlined /> Home
+                    </a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key={PROFILE_URL} className="menu-item">
+                  <Link href={PROFILE_URL} passHref>
+                    <a href={PASS_HREF}>
+                      <UserOutlined /> 프로필
+                    </a>
+                  </Link>
+                </Menu.Item>
+              </Menu>
+            </div>
+          </Col>
+          {filterGroup ? (
+            <>
+              <Col xs={24} sm={24} md={24} lg={8}>
+                {children}
+              </Col>
+              <Col className="filter" xs={24} sm={24} md={24} lg={8}>
+                <div className="content">{filterGroup}</div>
+              </Col>
+            </>
+          ) : (
+            <Col xs={24} sm={24} md={24} lg={16}>
               {children}
             </Col>
-            <Col xs={24} sm={24} md={24} lg={7} style={{ paddingTop: 12 }} />
-          </Row>
-        </div>
+          )}
+        </Row>
       </Content>
-    </Layout>
+    </StyledLayout>
   );
 };
 
