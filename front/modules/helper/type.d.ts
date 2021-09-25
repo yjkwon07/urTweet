0ad5b3716794708ff94319705300a6265e9f8064
yjkwon07 <dudwns9073@nate.com>
2021-09-25 14:39:14 +0900

    [front]: remove folder

--- front/modules/helper/type.d.ts
+++
@@ -1,3 +0,0 @@
-import { ActionCreatorWithPreparedPayload } from '@reduxjs/toolkit';
-
-export type ActionMetaPayload<P, M> = ActionCreatorWithPreparedPayload<[payload: P, meta?: M], P, string, never, M>;
--- front/modules/store/sagas.ts
+++
@@ -1,7 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import userSaga from '@modules/user/saga';
-
-export default function* rootSaga() {
-  yield all([fork(userSaga)]);
-}
