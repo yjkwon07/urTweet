8821d2ad72bbab79a0360af80a8866f4ac95694d
yjkwon07 <dudwns9073@nate.com>
2021-10-03 15:32:40 +0900

    [component]: react-hook-form@^7.7 - v6  => v7 - image-preview 깨짐 수정

--- front/views/Post/ListRead/PostForm/index.tsx
+++ front/views/Post/ListRead/PostForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FileImageTwoTone } from '@ant-design/icons';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -6,8 +6,7 @@ import { Button, Form, Input, message, Image } from 'antd';
 import { Controller, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 
-import { fileUpload } from '@modules/file';
-import imageDownloadLink from '@modules/file/utils/imageDownloadLink';
+import { fileUpload, imageDownloadLink } from '@modules/file';
 import { postAction, EDIT_POST_SCHEMA } from '@modules/post';
 import { FormEditPost } from '@modules/post/@types';
 import isCustomAxiosError from '@utils/isCustomAxiosError';
@@ -19,66 +18,76 @@ const PostForm = () => {
 
   const {
     control,
+    register,
+    watch,
+    getValues,
+    setValue,
     handleSubmit: checkSubmit,
-    errors,
+    formState: { errors },
     reset,
   } = useForm<FormEditPost>({
     mode: 'onSubmit',
     resolver: yupResolver(EDIT_POST_SCHEMA),
-    defaultValues: { content: '' },
+    defaultValues: { content: '', image: [] },
   });
 
-  const [imagePathList, setImagePathList] = useState<string[]>([]);
+  const imagePathList = watch('image');
   const imageInput = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = useCallback(
-    async (formData) => {
+  const handleSubmitCreatePost = useCallback(
+    async (formData: FormEditPost) => {
       try {
-        await dispatch(postAction.createPost.asyncThunk({ content: formData.content, image: imagePathList }));
+        await dispatch(postAction.createPost.asyncThunk(formData));
       } catch (error) {
         if (isCustomAxiosError(error)) {
           message.error(JSON.stringify(error.response.data.resMsg));
         }
       } finally {
         reset();
-        setImagePathList([]);
       }
     },
-    [dispatch, imagePathList, reset],
+    [dispatch, reset],
   );
 
   const handleClickImageUpload = useCallback(() => {
     if (imageInput.current) imageInput.current.click();
   }, []);
 
-  const handleChangeImage = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length >= 5) {
-      message.info('이미지는 최대 4개까지 허용됩니다.');
-      return;
-    }
-    try {
-      if (e.target.files && e.target.files.length) {
-        const filePathList = await fileUpload(e.target.files);
-        setImagePathList(filePathList);
-        if (imageInput.current) imageInput.current.value = '';
+  const handleChangeImage = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length >= 5) {
+        message.info('이미지는 최대 4개까지 허용됩니다.');
+        return;
       }
-    } catch (error) {
-      if (isCustomAxiosError(error)) {
-        message.error(JSON.stringify(error.response.data.resMsg));
+      try {
+        if (e.target.files && e.target.files.length) {
+          const filePathList = await fileUpload(e.target.files);
+          setValue('image', filePathList);
+          if (imageInput.current) imageInput.current.value = '';
+        }
+      } catch (error) {
+        if (isCustomAxiosError(error)) {
+          message.error(JSON.stringify(error.response.data.resMsg));
+        }
       }
-    }
-  }, []);
+    },
+    [setValue],
+  );
 
   const handleRemoveImage = useCallback(
-    (filePath) => () => {
-      setImagePathList((data) => data.filter((name) => name !== filePath));
+    (filePath: string) => () => {
+      const prevImage = getValues('image');
+      setValue(
+        'image',
+        prevImage?.filter((name) => name !== filePath),
+      );
     },
-    [],
+    [getValues, setValue],
   );
 
   return (
     <StyledCard>
-      <StyledForm encType="multipart/form-data" onSubmitCapture={checkSubmit(handleSubmit)}>
+      <StyledForm encType="multipart/form-data" onSubmitCapture={checkSubmit(handleSubmitCreatePost)}>
         <div className="content">
           <Form.Item
             className="form"
@@ -89,16 +98,25 @@ const PostForm = () => {
           >
             <Controller
               control={control}
-              as={<Input.TextArea maxLength={140} autoSize={{ minRows: 3, maxRows: 5 }} showCount defaultValue="" />}
               name="content"
-              id="content"
-              placeholder="게시글을 작성해 주세요."
+              render={({ field: { value, onChange } }) => (
+                <Input.TextArea
+                  id="content"
+                  maxLength={140}
+                  autoSize={{ minRows: 3, maxRows: 5 }}
+                  showCount
+                  value={value}
+                  onChange={onChange}
+                  placeholder="게시글을 작성해 주세요."
+                />
+              )}
             />
           </Form.Item>
         </div>
 
         <div className="actions">
-          <input type="file" name="image" multiple hidden ref={imageInput} onChange={handleChangeImage} />
+          <input type="hidden" {...register('image')} />
+          <input type="file" multiple hidden ref={imageInput} onChange={handleChangeImage} />
           <Button className="file" shape="circle" onClick={handleClickImageUpload} icon={<FileImageTwoTone />} />
           <Button className="submit" type="primary" shape="round" htmlType="submit">
             Tweet
@@ -107,7 +125,7 @@ const PostForm = () => {
 
         <div className="image_preview">
           <Image.PreviewGroup>
-            {imagePathList.map((filePath) => (
+            {imagePathList?.map((filePath) => (
               <div key={filePath} className="wrapper">
                 <Image width={150} height={150} src={imageDownloadLink(filePath, true)} alt="" />
                 <div className="button_wrapper">
--- front/views/Post/ListRead/styles.ts
+++ front/views/Post/ListRead/styles.ts
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
 
 export const StyledFilter = styled.div`
-  .wrapper {
-    width: 100%;
-    .select {
-      width: 120px;
-    }
-    .search {
-      border-radius: 17px;
+  & {
+    .wrapper {
+      width: 100%;
+      .select {
+        width: 120px;
+      }
+      .search {
+        border-radius: 17px;
+      }
     }
   }
 `;
