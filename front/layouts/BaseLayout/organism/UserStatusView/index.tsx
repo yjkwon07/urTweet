0c7f2889bc3faba08e4c36006cc7597b452644a0
yjkwon07 <dudwns9073@nate.com>
2021-10-09 19:31:34 +0900

    [component]: Did not expect server HTML to contain Compnent 해결 - 하나의 컴포넌트로 묶어서 처리 - 분기를 줄 경우 하나의 컨포넌트가아니기 때문에 싱크가 안맞는다는 버그가 나옴

--- front/layouts/BaseLayout/organism/UserStatusView/index.tsx
+++ front/layouts/BaseLayout/organism/UserStatusView/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useReadMyUser } from '@modules/user';
+import { getUserId } from '@utils/auth';
 
 import LoginForm from '../LoginForm';
 import UserProfile from '../UserProfile';
@@ -8,11 +9,15 @@ import UserSkeleton from '../UserSkeleton';
 
 const UserStatusView = () => {
   const { status: myDataStatus, data: myData } = useReadMyUser();
+  const isLogin = getUserId();
 
-  if (myDataStatus === 'SUCCESS' || myData) return <UserProfile />;
-  if (myDataStatus === 'LOADING') return <UserSkeleton />;
-  if (myDataStatus === 'FAIL' || !myData) return <LoginForm />;
-  return null;
+  return (
+    <>
+      {myData && <UserProfile />}
+      {myDataStatus === 'LOADING' && <UserSkeleton />}
+      {!isLogin && !myData && <LoginForm />}
+    </>
+  );
 };
 
 export default UserStatusView;
