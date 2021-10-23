1a2363f3153a4d86167f463dc9bfd26afef098f5
yjkwon07 <dudwns9073@nate.com>
2021-10-23 18:35:03 +0900

    [module]: createRequestAction 정책 변경 - requestAPI 제거   - 리턴의 의미가 action인데 부가적으로 넣는 느낌이 강해졌음 - F, M의 타입 의미 강화

--- front/modules/helper/createRequestAction.ts
+++ front/modules/helper/createRequestAction.ts
@@ -1,23 +1,25 @@
 import { createAction } from '@reduxjs/toolkit';
-import { AxiosResponse } from 'axios';
 
 import { createRequestAsyncThunk } from './createRequestAsyncThunk';
 import { RequestCommonMeta } from './type';
 
-export const createRequestAction = <R, S, M extends RequestCommonMeta>(
+export const createRequestAction = <
+  R,
+  S,
+  F extends ErrorCommonRes = ErrorCommonRes,
+  M extends RequestCommonMeta = RequestCommonMeta,
+>(
   type: string,
-  requestAPI: (query: R) => Promise<AxiosResponse<S>>,
 ) => {
   const REQUEST = `${type}/request`;
   const SUCCESS = `${type}/success`;
-  const FAILURE = `${type}/fail`;
+  const FAILURE = `${type}/failure`;
 
   const action = {
     TYPE: type,
     request: createAction(REQUEST, (payload: R, meta?: M) => ({ payload, meta })),
     success: createAction(SUCCESS, (payload: S, meta?: M) => ({ payload, meta })),
-    failure: createAction(FAILURE, (payload: AxiosResponse, meta?: M) => ({ payload, meta })),
-    requestAPI,
+    failure: createAction(FAILURE, (payload: F, meta?: M) => ({ payload, meta })),
   };
   const asyncThunk = createRequestAsyncThunk<R, S, M>(action.request);
   return { ...action, asyncThunk };
