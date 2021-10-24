bb4dbbe06e53a493afaead9b86d1b0075ceb4130
yjkwon07 <dudwns9073@nate.com>
2021-10-24 18:04:40 +0900

    [component]: UserStatusView - 페이지 렌딩시 무조건 LoginForm이 나타나지 않고, 이전 로그인 기록이 있으면 LoginForm은 보여주지 않게 설정

--- front/layouts/BaseLayout/UserStatusView/index.tsx
+++ front/layouts/BaseLayout/UserStatusView/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
+import { FetchStatus } from '@modules/fetchStatus';
 import { useReadMyUser } from '@modules/user';
+import { getUserId } from '@utils/auth';
 
 import LoginForm from '../LoginForm';
 import UserProfile from '../UserProfile';
 import UserSkeleton from '../UserSkeleton';
 
+const isShowUserSkeleton = (status: FetchStatus) => status === 'LOADING';
+const isShowUserProfile = (status: FetchStatus, isLogin: boolean) => status !== 'LOADING' && isLogin;
+const isShowLoginForm = (status: FetchStatus, isLogin: boolean, isPrevLogin: boolean) =>
+  status !== 'LOADING' && !isLogin && !isPrevLogin;
+
 const UserStatusView = () => {
   const { status: myDataStatus, data: myData } = useReadMyUser();
+  const isPrevLogin = getUserId();
 
   return (
     <>
-      {myDataStatus === 'LOADING' && <UserSkeleton />}
-      {myDataStatus !== 'LOADING' && myData ? <UserProfile /> : <LoginForm />}
+      {isShowUserSkeleton(myDataStatus) && <UserSkeleton />}
+      {isShowUserProfile(myDataStatus, !!myData) && <UserProfile />}
+      {isShowLoginForm(myDataStatus, !!myData, !!isPrevLogin) && <LoginForm />}
     </>
   );
 };
